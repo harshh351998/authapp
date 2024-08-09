928aa4b0ea1ea981dfac6cfe294411ca92feb6fa
harshh351998 <mendapara.harsh47@gmail.com>
2024-08-09 11:18:29 +0530

    Added register and login functionalities.

--- src/app/_service/user.service.ts
+++ src/app/_service/user.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { loginresp, registerconfirm, usercred, userregister } from '../model/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  baseUrl = environment.apiUrl;
+
+  _registerresp = signal<registerconfirm>({
+    userid: 0,
+    username: '',
+    otptext: ''
+  });
+
+  Userregistration(_data: userregister) {
+    return this.http.post(this.baseUrl + 'User/userregisteration', _data);
+  }
+
+  Confirmregisteration(_data: registerconfirm) {
+    let params = new HttpParams()
+      .set('userid', _data.userid.toString())
+      .set('username', _data.username)
+      .set('otptext', _data.otptext);
+
+    return this.http.post(this.baseUrl + 'User/confirmregisteration', null, { params: params });
+  }
+
+  Proceedlogin(_data: usercred) {
+    return this.http.post<loginresp>(this.baseUrl + 'Authorize/GenerateToken', _data);
+  }
 }
---
+++ src/app/model/user.model.ts
@@ -0,0 +1,25 @@
+export interface userregister {
+        userName: string;
+        name: string;
+        email: string;
+        phone: string;
+        password: string;  
+}
+
+export interface registerconfirm {
+        userid: number;
+        username: string;
+        otptext: string;
+    } 
+
+
+    export interface usercred {
+        username: string;
+        password: string;
+    }
+    
+    export interface loginresp {
+        token: string;
+        refreshToken: string;
+        userRole: string;
+    }
